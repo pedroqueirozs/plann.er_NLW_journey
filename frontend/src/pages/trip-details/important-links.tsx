30a6234a6ac4bb3a13e1ff2c795af9324daaf2aa
pedro <douglas.333.goncalves@gmail.com>
2024-07-19 12:38:43 -0300

    The functionality of adding links has been completed

--- frontend/src/pages/trip-details/important-links.tsx
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
 interface Links {
-  links:{
-    id: string;
-    title: string;
-    url: string;
-  }[];
+  id: string;
+  title: string;
+  url: string;
+
 }
 
 export function ImportantLinks() {
@@ -19,39 +18,28 @@ export function ImportantLinks() {
       .then((response) => setLinks(response.data.links));
   }, [tripId]);
 
-  console.log(typeof links)
-
   return (
     <div className="space-y-6">
-    {links.map((category)=>{
-      return(
-       <div className="space-y-5 ">
-         <div className="flex items-center justify-between gap-4">
-           <div className="space-y-1.5 ">
-    
-             <span className="block font-medium text-zinc-100">
-               {/* {links.title} */}
-               Titulo
-               
-             </span>
-             <a
-               href="#"
-               className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-             >
-              {/* {links.url} */}
-              Url
-              
-             </a>
-           </div>
-           <Link2 className="text-zinc-400 size-5 shrink-0" />
-         </div>
-       </div>  
-      )
-    })}
-      
+      {links.length > 0 ? (
+        links.map((link) => (
+          <div key={link.id} className="space-y-5">
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">{link.title}</span>
+                <a
+                  href={link.url}
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há links cadastrados</p>
+      )}
     </div>
-  );
+  )
 }
-
-
-  
